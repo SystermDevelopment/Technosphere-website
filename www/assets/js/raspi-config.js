293391f7cf119364cae267d7e8b7e969ddb68b8d
c_yara <c_yara@technosphere.co.jp>
2025-05-02 13:58:24 +0900

    本番環境のMixed Content回避のためproxy対応

--- www/assets/js/raspi-config.js
+++ www/assets/js/raspi-config.js
@@ -1,6 +1,9 @@
 // raspi-config.js
+const poseServerUrl = 'http://pose.technosphere.co.jp/';
+const proxyUrl = `/proxy.php?target=${encodeURIComponent(poseServerUrl)}`;
+
 const ENV_CONFIG = {
-    rasPiPico_apiUrl: 'http://pose.technosphere.co.jp/',
+    rasPiPico_apiUrl: proxyUrl,
     //rasPiPico_apiUrl: 'http://192.168.11.204/',
     national_holidays_apiUrl: `https://api.national-holidays.jp/${new Date().getFullYear()}`,
     holidays: [
